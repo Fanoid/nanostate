d3fead9fc89bf8ef2d43bde5d49893fce02f9159
Fan Hong <hongfanxo@gmail.com>
2015-11-18 11:30:10 +0800

    tmp

--- pynanostate/nanostate.py
+++ pynanostate/nanostate.py
@@ -1,24 +1,31 @@
 from __future__ import print_function
 import nanomsg
-from nanomsg import wrapper as nn_wrapper
 import msgpack
 import sys
 
 class Nanostate:
     def __init__(self, identity, addr):
-        self._sock = nn_wrapper.nn_socket(nanomsg.AF_SP, nanomsg.BUS)
-        nn_wrapper.nn_connect(self._sock, addr)
+        self._sock = nanomsg.Socket(nanomsg.BUS)
+        self._sock.connect(addr)
         self._identity = identity
 
     def send_state_update(self, name, data):
         buf = msgpack.packb(self._identity, name, data)
-        nn_wrapper.nn_send(self._sock, buf, 0)
+        self._sock.send(buf)
 
     def recv_state_update(self):
-        result, buf = nn_wrapper.nn_recv(self._sock, 0)
+        buf = self._sock.recv()
         return msgpack.unpackb(buf)
+
+    def has_state_update(self): # Not work
+        return (self._sock.recv_fd == 
+                self._sock.get_int_option(nanomsg.SOL_SOCKET, nanomsg.RCVFD))
 
 nanostate = Nanostate(sys.argv[1], sys.argv[2])
 while True:
-    sender, name, data = nanostate.recv_state_update()
-    print(sender, name, data)
+    if (nanostate.has_state_update()):
+        print("received!")
+        sender, name, data = nanostate.recv_state_update()
+        print(sender, name, data)
+    else:
+        print("No!")
