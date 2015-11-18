9c4db1c674360281e6a6690272a4d32abbbf2782
Fan Hong <hongfanxo@gmail.com>
2015-11-18 16:26:09 +0800

    improve python version

--- pynanostate/nanostate.py
+++ pynanostate/nanostate.py
@@ -1,31 +1,39 @@
 from __future__ import print_function
+from nanomsg import *
 import nanomsg
 import msgpack
 import sys
+import time
 
 class Nanostate:
     def __init__(self, identity, addr):
-        self._sock = nanomsg.Socket(nanomsg.BUS)
+        self._sock = Socket(BUS)
         self._sock.connect(addr)
+        self._sock.recv_timeout = 50
         self._identity = identity
 
     def send_state_update(self, name, data):
-        buf = msgpack.packb(self._identity, name, data)
-        self._sock.send(buf)
+        buf = msgpack.packb((self._identity, name, data))
+        try:
+            self._sock.send(buf)
+            return True
+        except NanoMsgError:
+            return False
 
     def recv_state_update(self):
-        buf = self._sock.recv()
-        return msgpack.unpackb(buf)
+        state = None
+        try:
+            buf = self._sock.recv()
+            state = msgpack.unpackb(buf, use_list = False)
+            return (True, state)
+        except NanoMsgError:
+            return (False, state)
 
-    def has_state_update(self): # Not work
-        return (self._sock.recv_fd == 
-                self._sock.get_int_option(nanomsg.SOL_SOCKET, nanomsg.RCVFD))
-
-nanostate = Nanostate(sys.argv[1], sys.argv[2])
-while True:
-    if (nanostate.has_state_update()):
-        print("received!")
-        sender, name, data = nanostate.recv_state_update()
-        print(sender, name, data)
-    else:
-        print("No!")
+if __name__ == "__main__":
+    nanostate = Nanostate(sys.argv[1], sys.argv[2])
+    while True:
+        time.sleep(1)
+        nanostate.send_state_update("Hello", "World")
+        result, state = nanostate.recv_state_update()
+        if result:
+            print(state)
