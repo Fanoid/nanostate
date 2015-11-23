cbe42af255770297d187f2c77ea799502b0d9bba
Fan Hong <hongfanxo@gmail.com>
2015-11-23 13:54:56 +0800

    fix ws-hub

--- pynanostate/echo_client.py
+++ pynanostate/echo_client.py
@@ -2,6 +2,8 @@
 from ws4py.client.threadedclient import WebSocketClient
 import msgpack
 
+import argparse
+
 class EchoClient(WebSocketClient):
     def received_message(self, m):
         print(type(m))
@@ -11,10 +13,18 @@ class EchoClient(WebSocketClient):
             print(state)
         else:
             print(str(m))
+            self.send("World!")
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="A WebSocket echo client.")
+    parser.add_argument("--connect", dest = "addr", required = True, help = "The address of WebSocket server, such as ws://127.0.0.1:15001/ws")
+
+    args = parser.parse_args()
+    print(args)
+    addr = args.addr
+
     try:
-        ws = EchoClient('ws://127.0.0.1:9000/ws', protocols=['http-only', 'chat'])
+        ws = EchoClient(addr, protocols=['http-only', 'chat'])
         ws.connect()
         ws.run_forever()
     except KeyboardInterrupt:
---
+++ pynanostate/hub-ws.py
@@ -0,0 +1,81 @@
+from __future__ import print_function
+from nanomsg import *
+
+import cherrypy # to host a websocket hub
+
+from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
+from ws4py.websocket import WebSocket
+from ws4py.messaging import TextMessage
+
+import argparse
+
+parser = argparse.ArgumentParser(description="A nanostate hub with router between websocket written by Python.")
+parser.add_argument('--bind', '-B', dest = "addr", required = True, help = "The address to bind, which should be compatible with nanomsg, such as 'tcp://127.0.0.1:15000'")
+
+parser.add_argument("--ws_addr", dest = "ws_addr", required = True, help = "The address to bind websocket server, such as 127.0.0.1")
+parser.add_argument("--ws_port", dest = "ws_port", type=int, required = True, help = "The port to bind websocket server, such as 15001")
+
+args = parser.parse_args()
+print(args)
+addr = args.addr
+ws_addr = args.ws_addr
+ws_port = args.ws_port
+
+class WebSocketHandler(WebSocket):
+    def received_message(self, msg):
+        print("WebSocket received: " + str(msg))
+        cherrypy.engine.publish('websocket-broadcast', msg)
+
+        if (msg.is_binary):
+            buf = msg.data
+            sock.send(buf)
+        else:
+            sock.send(str(msg))
+
+class Root:
+    def __init__(self, addr, port):
+        self.addr = addr
+        self.port = port
+
+    @cherrypy.expose
+    def index(self):
+        return "Use " + self.addr + ":" + str(self.port) + " for WebSocket connection."
+
+    @cherrypy.expose
+    def ws(self):
+        cherrypy.log("Handler created: %s" % repr(cherrypy.request.ws_handler))
+
+
+# Initialize websocket hub
+cherrypy.config.update({
+    'server.socket_host': ws_addr,
+    'server.socket_port': ws_port})
+WebSocketPlugin(cherrypy.engine).subscribe()
+cherrypy.tools.websocket = WebSocketTool()
+
+cherrypy.tree.mount(Root(ws_addr, ws_port), "/", config={
+    '/ws': {
+        'tools.websocket.on': True,
+        'tools.websocket.handler_cls': WebSocketHandler
+        }
+    })
+
+# Start WebSocket hub
+print("Starting WebSocket hub")
+cherrypy.engine.start()
+
+# Initialize nanostate hub
+sock = Socket(BUS)
+sock.bind(args.addr)
+print("Nanostate socket bound to " + addr)
+
+# Start nanostate hub
+print("Starting nanostate hub")
+while True:
+    buf = sock.recv()
+    print("nanostate received: " + str(buf))
+    sock.send(buf)
+    cherrypy.engine.publish('websocket-broadcast', buf)
+
+sock.close()
+cherrypy.engine.stop()
