054ace705bd303231d64bd0824d52ea1db040aea
Fan Hong <hongfanxo@gmail.com>
2016-02-14 14:40:06 +0800

    improve nodenanostate

--- nodenanostate/hub-ws.js
+++ nodenanostate/hub-ws.js
@@ -1,22 +1,22 @@
-var nano = require('nanomsg');
-var argparse = require('argparse')
-var WebSocketServer = require('ws').Server
+var nano = require("nanomsg");
+var argparse = require("argparse")
+var WebSocketServer = require("ws").Server
 
 var parser = argparse.ArgumentParser({
-  description: "A nanostate hub with router between websocket written by Node.js."
+  description: "A nanostate hub with route to/from websocket written by Node.js."
 });
 parser.addArgument(
-    ['--bind', '-B'],
+    ["--bind", "-B"],
     {
       dest: "addr",
-      required: true,
+      defaultValue: "tcp://127.0.0.1:15000",
       help: "The address to bind, which should be compatible with nanomsg, such as 'tcp://127.0.0.1:15000'"
     });
 parser.addArgument(
     ["--ws_host"],
     {
       dest: "ws_host",
-      required: true,
+      defaultValue: "127.0.0.1",
       help: "The address to bind websocket server, such as 127.0.0.1"
     });
 parser.addArgument(
@@ -24,7 +24,7 @@ parser.addArgument(
     {
       dest: "ws_port",
       type: "int",
-      required: true,
+      defaultValue: 15001,
       help: "The port to bind websocket server, such as 15001"
     });
 
@@ -33,19 +33,28 @@ var addr = args.addr;
 var ws_host = args.ws_host;
 var ws_port = args.ws_port;
 
-var sock = nano.socket('bus');
+// Start socket hub
+var sock = nano.socket("bus");
 sock.bind(addr);
 sock.sndbuf(1024 * 1024 * 1024);
 sock.rcvbuf(1024 * 1024 * 1024);
 sock.rcvmaxsize(-1);
+
+sock.on("data", function (buf) {
+//  console.log("sock received: %s", buf);
+  console.log(sock)
+  sock.send(buf);
+  wss.broadcast(buf);
+});
+
 console.log("Socket bound to " + addr);
 
+// Start WebSocket hub
 var wss = new WebSocketServer({
   host: ws_host,
   port: ws_port,
   path: "/ws"
 });
-console.log("WebSocket bound to " + ws_host + ":" + ws_port);
 
 wss.broadcast = function broadcast(data) {
   wss.clients.forEach(function each(client) {
@@ -53,18 +62,14 @@ wss.broadcast = function broadcast(data) {
   });
 };
 
-wss.on('connection', function connection(ws) {
-  console.log("1 new client connected.");
-  ws.on('message', function(data, flags) {
-//    console.log('ws received: %s', data);
+wss.on("connection", function connection(ws) {
+  console.log("1 WebSocket client connected.");
+  ws.on("message", function(data, flags) {
+//    console.log("ws received: %s", data);
     wss.broadcast(data);
     sock.send(data);
   });
 });
 
-sock.on('data', function (buf) {
-//  console.log('sock received: %s', buf);
-  sock.send(buf);
-  wss.broadcast(buf);
-});
+console.log("WebSocket bound to http://" + ws_host + ":" + ws_port + "/ws");
 
--- nodenanostate/hub.js
+++ nodenanostate/hub.js
@@ -1,29 +1,30 @@
-var nano = require('nanomsg');
-var argparse = require('argparse')
+var nano = require("nanomsg");
+var argparse = require("argparse")
 
 var parser = argparse.ArgumentParser({
   description: "A nanostate hub written by Node.js."
 });
 parser.addArgument(
-    ['--bind', '-B'],
+    ["--bind", "-B"],
     {
       dest: "addr",
-      required: true,
+      defaultValue: "tcp://127.0.0.1:15000",
       help: "The address to bind, which should be compatible with nanomsg, such as 'tcp://127.0.0.1:15000'"
     });
 
 var args = parser.parseArgs()
 var addr = args.addr;
 
-var sock = nano.socket('bus');
-
+// Start socket hub
+var sock = nano.socket("bus");
 sock.bind(addr);
 sock.sndbuf(1024 * 1024 * 1024);
 sock.rcvbuf(1024 * 1024 * 1024);
 sock.rcvmaxsize(-1);
 
-console.log("Socket bound to " + addr);
-
-sock.on('data', function (buf) {
+sock.on("data", function (buf) {
   sock.send(buf);
 });
+
+console.log("Socket bound to " + addr);
+
